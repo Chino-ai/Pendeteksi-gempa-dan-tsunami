ff4a5bc1c74d22ffd3b3a09e84b434e01cc3a0d2
Ilham Maulana <maulanailham510@gmail.com>
2021-11-24 10:13:09 +0700

    -menambahkan activity gempa -gambar "logosplashscreen" -menambahkan kode di  Activity SplashScreen

---
+++ app/src/main/java/com/example/pendeteksigempadantsunami/ui/gempa/GempaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pendeteksigempadantsunami.ui.gempa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pendeteksigempadantsunami.R
+
+class GempaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gempa)
+    }
+}
