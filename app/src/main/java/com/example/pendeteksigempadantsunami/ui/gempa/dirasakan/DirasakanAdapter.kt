e2add683f0116cbc17659c1fd9f40278269b594e
Ilham Maulana <maulanailham510@gmail.com>
2021-11-24 13:59:02 +0700

    menambakan kelas "Dirasakan Adapter" , "ManitudoAdapter" dan  "TerkiniAdapter"

---
+++ app/src/main/java/com/example/pendeteksigempadantsunami/ui/gempa/dirasakan/DirasakanAdapter.kt
@@ -0,0 +1,15 @@
+package com.example.pendeteksigempadantsunami.ui.gempa.dirasakan
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pendeteksigempadantsunami.databinding.ItemDirasakanBinding
+
+class DirasakanAdapter  {
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/pendeteksigempadantsunami/ui/gempa/magnitudo/MagnitudoAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.pendeteksigempadantsunami.ui.gempa.magnitudo
+
+class MagnitudoAdapter {
+}
