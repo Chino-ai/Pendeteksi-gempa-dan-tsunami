71cf8bfdc6dfb9958956c674f7c248172146e0c6
Ilham Maulana <maulanailham510@gmail.com>
2021-11-24 10:52:20 +0700

    mengedit strings.xml,colors dan activity gempa

--- app/src/main/java/com/example/pendeteksigempadantsunami/ui/gempa/GempaActivity.kt
+++ app/src/main/java/com/example/pendeteksigempadantsunami/ui/gempa/GempaActivity.kt
@@ -3,10 +3,21 @@ package com.example.pendeteksigempadantsunami.ui.gempa
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pendeteksigempadantsunami.R
+import com.example.pendeteksigempadantsunami.databinding.ActivityGempaBinding
+import com.example.pendeteksigempadantsunami.ui.sectionpager.SectionPager
 
 class GempaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gempa)
+        gempaBinding = ActivityGempaBinding.inflate(layoutInflater)
+        setContentView(gempaBinding.root)
+
+        sectionPagerAdapter = SectionPager(this, supportFragmentManager)
+        gempaBinding.viewPager.adapter = sectionPagerAdapter
+        gempaBinding.tabs.setupWithViewPager(gempaBinding.viewPager)
+        supportActionBar?.elevation = 0f
     }
+
+    private lateinit var gempaBinding: ActivityGempaBinding
+    private lateinit var sectionPagerAdapter: SectionPager
 }
