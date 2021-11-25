51268fe1b3740bb00f47c3f72d47f86db5018b17
Ilham Maulana <maulanailham510@gmail.com>
2021-11-25 12:46:43 +0700

    Menambahkan file kelas DirasakanEntity, MagnitudoEntity dan TerkiniEntity

---
+++ app/src/main/java/com/example/pendeteksigempadantsunami/data/local/DirasakanEntity.kt
@@ -0,0 +1,11 @@
+package com.example.pendeteksigempadantsunami.data.local
+
+data class DirasakanEntity (
+    val magnitudo : String,
+    val Kedalamann : String,
+    val waktu : String,
+    val alamat : String,
+    val dirasakan: String
+
+)
+
---
+++ app/src/main/java/com/example/pendeteksigempadantsunami/data/local/MagnitudoEntity.kt
@@ -0,0 +1,10 @@
+package com.example.pendeteksigempadantsunami.data.local
+
+data class MagnitudoEntity (
+    val magnitudo : String,
+    val Kedalamann : String,
+    val waktu : String,
+    val alamat : String,
+    val potensiTsunami: String
+
+)
