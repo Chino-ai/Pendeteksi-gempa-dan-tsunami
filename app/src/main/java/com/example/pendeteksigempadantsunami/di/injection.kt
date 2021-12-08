a6f8c27ed9347a55f3ae5b052e5719d5c6722cc0
Ilham Maulana <maulanailham510@gmail.com>
2021-12-08 11:04:44 +0700

    mengedit file gradle , android manifiest , injection dan layout activity_edukasi

--- app/src/main/java/com/example/pendeteksigempadantsunami/di/injection.kt
+++ app/src/main/java/com/example/pendeteksigempadantsunami/di/injection.kt
@@ -3,11 +3,12 @@ package com.example.pendeteksigempadantsunami.di
 import android.content.Context
 import com.example.pendeteksigempadantsunami.data.remote.GempaRepository
 import com.example.pendeteksigempadantsunami.data.remote.RemoteDataSource
+import com.example.pendeteksigempadantsunami.utils.AppExecutors
 
-import com.example.submissionarchitecturecomponent.utils.AppExecutors
+
 
 object Injection {
-    fun provideRepository(): GempaRepository {
+    fun provideRepository(context: Context): GempaRepository {
 
         val remoteDataSource = RemoteDataSource.getInstance()
 
