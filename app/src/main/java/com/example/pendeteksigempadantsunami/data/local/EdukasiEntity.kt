c74b031881d1afe86288bd8b79a624dc847ee184
Ilham Maulana <maulanailham510@gmail.com>
2021-12-08 11:00:50 +0700

    membuat kelas EdukasiEntitty

---
+++ app/src/main/java/com/example/pendeteksigempadantsunami/data/local/EdukasiEntity.kt
@@ -0,0 +1,9 @@
+package com.example.pendeteksigempadantsunami.data.local
+
+class EdukasiEntity (
+       val ID : String,
+       val title : String,
+       val content : String,
+       val image : String
+
+)
