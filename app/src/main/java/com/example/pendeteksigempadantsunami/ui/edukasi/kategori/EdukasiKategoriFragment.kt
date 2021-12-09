0895af0e3c1414914b2a8c608dc046022fb4939d
Ilham Maulana <maulanailham510@gmail.com>
2021-12-09 10:38:01 +0700

    Menambahkan Kelas Fragment EdukasiKategori dan EdukasiPilihan

---
+++ app/src/main/java/com/example/pendeteksigempadantsunami/ui/edukasi/kategori/EdukasiKategoriFragment.kt
@@ -0,0 +1,31 @@
+package com.example.pendeteksigempadantsunami.ui.edukasi.kategori
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pendeteksigempadantsunami.R
+import com.example.pendeteksigempadantsunami.databinding.FragmentEdukasiKategoriBinding
+
+
+class EdukasiKategoriFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        edukasiKategoriBinding = FragmentEdukasiKategoriBinding.inflate(layoutInflater)
+        return edukasiKategoriBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private lateinit var edukasiKategoriBinding: FragmentEdukasiKategoriBinding
+
+    }
